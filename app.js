17e9371bc349d1c5da10b53ae85565918c25dd5f
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2026-01-21 16:11:40 +0530

    Day 1 Display catMe output in browser

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+const express = require("express");
 const catMe = require("cat-me");
 
+const app = express();
+const PORT =  process.env.PORT || 3000; 
+// Define port (Render provides PORT, otherwise use 3000)
 
+app.get( '/', (req,res) => {
+    res.send(`<pre>${catMe()}</pre>`)
+})
+
+app.listen(PORT, ()=> {
+console.log(`Server running on http://localhost:${PORT}`)
+})
+
+// Print cat in the terminal
 console.log(catMe());
+
+// Open  http://localhost:3000 to see cat in Browser
